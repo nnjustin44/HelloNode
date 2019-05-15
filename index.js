885e4265ceea29a404fcee2e25e5e916befcdb31
Williamrobert Green <wkgreen13@gmail.com>
2019-05-14 20:22:49 -0400

    changes console log

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log("Welcome");
 sayHello("Justin");
 sayHello("Bubba");
 sayHello(process.argv[2]);
+
 function sayHello(userName) {
-  console.log("Hello " + userName);
+  console.log("Hello ", userName);
 }
