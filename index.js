de9bd4d27476ec3ca5dac033a5c6ddebc15e5452
nnjustin44 <nnjustin44@gmail.com>
2019-05-15 22:11:21 -0400

    practicing

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ console.log("Welcome");
 sayHello("Justin");
 sayHello("Bubba");
 sayHello(process.argv[2]);
-
+sayHello("John");
+sayHello("Qway");
 function sayHello(userName) {
   console.log("Hello ", userName);
 }
